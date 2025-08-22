b199b440dc6f6fbe28987942c8168745b6b149a5
Alisson Rodrigues <fiore_alisson@hotmail.com>
2025-08-22 00:10:58 -0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,25 +1,26 @@
 import json
 
-def converter_txt_para_json(nome_arquivo_txt, nome_arquivo_json):
+def converter_txt_para_json(n_arquivo_txt, n_arquivo_json):
     palavras = []
     try:
-        with open(nome_arquivo_txt, 'r', encoding='utf-8') as arquivo_txt:
+        with open(n_arquivo_txt, 'r', encoding='utf-8') as arquivo_txt:
             for linha in arquivo_txt:
                 palavra = linha.strip()
                 if palavra:
                     palavras.append(palavra)
 
-        with open(nome_arquivo_json, 'w', encoding='utf-8') as arquivo_json:
+        with open(n_arquivo_json, 'w', encoding='utf-8') as arquivo_json:
             json.dump(palavras, arquivo_json, ensure_ascii=False, indent=4)
         
-        print(f"Sucesso! O arquivo '{nome_arquivo_json}' foi criado com {len(palavras)} palavras.")
+        print(f"Sucesso! O arquivo '{n_arquivo_json}' foi criado com {len(palavras)} palavras.")
 
     except FileNotFoundError:
-        print(f"Erro: O arquivo '{nome_arquivo_txt}' não foi encontrado.")
+        print(f"Erro: O arquivo '{n_arquivo_txt}' não foi encontrado.")
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
 
-arquivo_de_entrada = 'palavras.txt'
-arquivo_de_saida = 'palavras_para_o_jogo.json'
+entrada = 'palavras.txt'
+saida = 'palavras_convertidas.json'
 
-converter_txt_para_json(arquivo_de_entrada, arquivo_de_saida)
+
+converter_txt_para_json(entrada, saida)
